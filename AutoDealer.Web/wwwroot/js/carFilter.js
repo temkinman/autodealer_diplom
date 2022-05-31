f6879d4703c7cd4f5ea99e144d12e28a42dd5965
Artyom Chegodar <a.chegodar@softteco.com>
2022-06-01 00:20:50 +0300

    changed styled

---
+++ AutoDealer.Web/wwwroot/js/carFilter.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    let companyValue = $("#CompanyId").val();
+
+    if (companyValue > 0) {
+        $("#ModelId").prop("disabled", false);
+    }
+});
+
+$("#CompanyId").on("change", function () {
+    let selection = $(this).find("option:selected").text();
+    let value = $(this).val();
+
+    if (value > 0) {
+        $.ajax({
+            url: '@Url.Action("getmodelsbycompany", "car")',
+            type: "GET",
+            data: {
+                companyId: value,
+                filter: true
+            },
+            success: function (data) {
+                $("#SelectModels").html(data);
+                $("#ModelId").addClass("dropdown_item_select checkout_input");
+            },
+            error: function (ex) {
+            }
+        });
+    }
+
+    if (value == 0) {
+        $("#ModelId").find('option').remove();
+        $("#ModelId").append('<option value="0">Выберите</options>');
+        $("#ModelId").prop("disabled", true);
+    }
+});
